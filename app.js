10b11743b2afb921f8a0763a52388474266aa83c
jose <josecarlosesparza@gmail.com>
2019-07-11 13:36:40 +0100

    blogSchema created

--- app.js
+++ app.js
@@ -3,10 +3,23 @@ var bodyParser = require('body-parser'),
     express = require('express'),
     app = express();
 
+//APP CONFIG
 mongoose.connect("mongodb://localhost:27017/blog_app", { useNewUrlParser: true });
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+
+//MONGOOSE/MODEL CONFIG
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+//RESTFUL ROUTES
 
 app.listen(3000, process.env.IP, function(){
     console.log("Blog App Server is running!");
