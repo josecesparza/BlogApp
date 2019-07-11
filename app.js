a0637af32f5905b215908602856f221ed4c55f2d
jose <josecarlosesparza@gmail.com>
2019-07-11 13:30:44 +0100

    app.js setup

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ var bodyParser = require('body-parser'),
     app = express();
 
 mongoose.connect("mongodb://localhost:27017/blog_app", { useNewUrlParser: true });
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(3000, process.env.IP, function(){
+    console.log("Blog App Server is running!");
+});
