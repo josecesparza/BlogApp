49465c1c39e2a7a38ac6bd98fa2c6be7303f6895
jose <josecarlosesparza@gmail.com>
2019-07-15 02:25:27 +0100

    Sanitize process added

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ app.get("/blogs/new", function (req, res) {
 
 //CREATE ROUTE
 app.post("/blogs", function (req, res) {
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     //Create blog
     Blog.create(req.body.blog, function (err, newBlog) {
         if (err) {
@@ -90,6 +91,8 @@ app.get("/blogs/:id/edit", function (req, res) {
 
 //UPDATE ROUTE
 app.put("/blogs/:id", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
         if(err){
             res.redirect("/blogs");
